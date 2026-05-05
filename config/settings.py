764c83b9e76911ad40e8eaae992540f454bfc8c5
difranalstudio <difranalstudio@gmail.com>
2026-05-04 23:31:12 -0300

    Configurações Deploy

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 SECRET_KEY = "django-insecure-9er@xo%)sysihd@a+whob^5@+49*!bvxt=!6h2@2xl+*a6%1w*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
